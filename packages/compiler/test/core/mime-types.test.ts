719c27325826f9c8071937d7b2fb938b7dc2e806
Cameron Taggart <cataggar@microsoft.com>
2025-06-23 11:30:37 +0200

    mime-types

--- packages/compiler/test/core/mime-types.test.ts
+++ packages/compiler/test/core/mime-types.test.ts
@@ -1,18 +1,18 @@
-import { deepStrictEqual, strictEqual } from "assert";
+import { assert } from "../../src/testing/system-assert.js";
 import { describe, it } from "vitest";
 import { parseMimeType } from "../../src/core/mime-type.js";
 
 describe("compiler: mime-type utils", () => {
   it("return undefined if invalid mime type", () => {
-    strictEqual(parseMimeType("foo/bar/baz"), undefined);
+    assert.strictEqual(parseMimeType("foo/bar/baz"), undefined);
   });
 
   it("parse simple mime types", () => {
-    deepStrictEqual(parseMimeType("application/json"), { type: "application", subtype: "json" });
+    assert.deepStrictEqual(parseMimeType("application/json"), { type: "application", subtype: "json" });
   });
 
   it("parse mime types with suffix", () => {
-    deepStrictEqual(parseMimeType("application/merge-patch+json"), {
+    assert.deepStrictEqual(parseMimeType("application/merge-patch+json"), {
       type: "application",
       subtype: "merge-patch",
       suffix: "json",
